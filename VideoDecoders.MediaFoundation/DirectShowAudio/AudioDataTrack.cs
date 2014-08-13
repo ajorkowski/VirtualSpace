561bfd117ec08d46722d05efd8cce10347507e94
Felix Jorkowski <felix.jorkowski@tigerpistol.com.au>
2014-08-13 17:41:26 +1000

    Try using some pre-made base directshow classes

---
+++ VideoDecoders.MediaFoundation/DirectShowAudio/AudioDataTrack.cs
@@ -0,0 +1,13 @@
+﻿using DirectShow.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoDecoders.MediaFoundation.DirectShowAudio
+{
+    public class AudioDataTrack : DemuxTrack
+    {
+    }
+}

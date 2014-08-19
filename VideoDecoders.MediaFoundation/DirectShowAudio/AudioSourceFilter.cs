9fac23a71cedad4722b7fcffbcb8de2d5d0f93cc
Felix Jorkowski <felix@jorkowski.com>
2014-08-19 17:05:51 +1000

    DD show changes

--- VideoDecoders.MediaFoundation/DirectShowAudio/AudioSourceFilter.cs
+++ VideoDecoders.MediaFoundation/DirectShowAudio/AudioSourceFilter.cs
@@ -1,20 +1,48 @@
 ﻿using DirectShow;
 using DirectShow.BaseClasses;
 using System;
+using System.Runtime.InteropServices;
 using MF = MediaFoundation;
 
 namespace VideoDecoders.MediaFoundation.DirectShowAudio
 {
     public class AudioSourceFilter : BaseSourceFilter
     {
-        private readonly MF.IMFMediaType _mediaType;
+        private readonly AMMediaType _mediaType;
 
         private long _lastSampleTime = 0;
 
         public AudioSourceFilter(MF.IMFMediaType mediaType)
             : base("Audio Source Filter")
         {
-            _mediaType = mediaType;
+            Guid subType;
+            mediaType.GetGUID(MF.MFAttributesClsid.MF_MT_SUBTYPE, out subType);
+            if (subType != MediaSubTypes.MEDIASUBTYPE_DTS)
+            {
+                throw new NotImplementedException();
+            }
+
+            MF.Misc.WaveFormatEx format;
+            int formatSize;
+            var hr = MF.MFExtern.MFCreateWaveFormatExFromMFMediaType(mediaType, out format, out formatSize, MF.MFWaveFormatExConvertFlags.Normal);
+            if (hr != S_OK) { throw new NotSupportedException(); }
+
+            var pwfx = new WaveFormatEx
+            {
+                cbSize = (ushort)format.cbSize,
+                nAvgBytesPerSec = format.nAvgBytesPerSec,
+                nBlockAlign = (ushort)format.nBlockAlign,
+                nChannels = (ushort)format.nChannels,
+                nSamplesPerSec = format.nSamplesPerSec,
+                wBitsPerSample = (ushort)format.wBitsPerSample,
+                wFormatTag = (ushort)format.wFormatTag
+            };
+
+            _mediaType = new AMMediaType();
+            _mediaType.majorType = MediaType.Audio;
+            _mediaType.subType = MediaFoundation.MediaSubTypes.MEDIASUBTYPE_DTS;
+            _mediaType.formatType = FormatType.WaveEx;
+            AMMediaType.SetFormat(ref _mediaType, ref pwfx);
         }
 
         //~AudioSourceFilter()
@@ -43,52 +71,26 @@ namespace VideoDecoders.MediaFoundation.DirectShowAudio
 
         public int GetMediaType(ref AMMediaType mediaType)
         {
-            Guid subType;
-            _mediaType.GetGUID(MF.MFAttributesClsid.MF_MT_SUBTYPE, out subType);
-            if (subType != MediaSubTypes.MEDIASUBTYPE_DTS)
-            {
-                throw new NotImplementedException();
-            }
-
-            MF.Misc.WaveFormatEx format;
-            int formatSize;
-            var hr = MF.MFExtern.MFCreateWaveFormatExFromMFMediaType(_mediaType, out format, out formatSize, MF.MFWaveFormatExConvertFlags.Normal);
-            if (hr != S_OK) { throw new NotSupportedException(); }
-
-            var pwfx = new WaveFormatEx
-            {
-                cbSize = (ushort)format.cbSize,
-                nAvgBytesPerSec = format.nAvgBytesPerSec,
-                nBlockAlign = (ushort)format.nBlockAlign,
-                nChannels = (ushort)format.nChannels,
-                nSamplesPerSec = format.nSamplesPerSec,
-                wBitsPerSample = (ushort)format.wBitsPerSample,
-                wFormatTag = (ushort)format.wFormatTag
-            };
-
-            mediaType.majorType = MediaType.Audio;
-            mediaType.subType = MediaFoundation.MediaSubTypes.MEDIASUBTYPE_DTS;
-            mediaType.formatType = FormatType.WaveEx;
-            AMMediaType.SetFormat(ref mediaType, ref pwfx);
-
+            mediaType.Set(_mediaType);
             return NOERROR;
         }
 
         public int DecideBufferSize(ref IMemAllocatorImpl pAlloc, ref AllocatorProperties prop)
         {
-            throw new NotImplementedException();
-            //AllocatorProperties _actual = new AllocatorProperties();
+            AllocatorProperties actual = new AllocatorProperties();
 
-            //BitmapInfoHeader _bmi = (BitmapInfoHeader)Pins[0].CurrentMediaType;
-            //prop.cbBuffer = _bmi.GetBitmapSize();
-            //if (prop.cbBuffer < _bmi.ImageSize)
-            //{
-            //    prop.cbBuffer = _bmi.ImageSize;
-            //}
-            //prop.cBuffers = 1;
+            var wfx = (WaveFormatEx)_mediaType;
+            if (wfx == null) return VFW_E_INVALIDMEDIATYPE;
 
-            //int hr = pAlloc.SetProperties(prop, _actual);
-            //return hr;
+            prop.cbBuffer = wfx.nAvgBytesPerSec;
+            if (prop.cbBuffer < wfx.nBlockAlign * wfx.nSamplesPerSec)
+            {
+                prop.cbBuffer = wfx.nBlockAlign * wfx.nSamplesPerSec;
+            }
+            prop.cbAlign = wfx.nBlockAlign;
+            prop.cBuffers = 3;
+            int hr = pAlloc.SetProperties(prop, actual);
+            return hr;
         }
 
         public int FillBuffer(ref IMediaSampleImpl _sample)
